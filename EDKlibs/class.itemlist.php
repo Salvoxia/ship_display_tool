3ec81b744863b6494df93c4b382049ad2bedfa7c
Salvoxia <Salvoxia@blindfish.info>
2015-11-10 16:33:53 +0100

    Updated ItemList library to include the new singleton flag

--- EDKlibs/class.itemlist.php
+++ EDKlibs/class.itemlist.php
@@ -84,46 +84,46 @@ class ItemList
 			return;
 		}
 		$sql = "select inv.icon, inv.typeID,
-				itp.price, kb3_item_types.*, dga.value as metalevel, dgb.value as techlevel,
-				dc.value as usedcharge, dl.value as usedlauncher,
-				inv.groupID, inv.typeName, inv.capacity, inv.raceID, inv.mass, inv.volume,
-				inv.basePrice, inv.marketGroupID";
+                            itp.price, kb3_item_types.*, dga.value as metalevel, dgb.value as techlevel,
+                            dc.value as usedcharge, dl.value as usedlauncher,
+                            inv.groupID, inv.typeName, inv.capacity, inv.raceID, inv.mass, inv.volume,
+                            inv.basePrice, inv.marketGroupID";
 		if (count($this->destroyedIDarray)) {
-			$sql .= ", if(dl.attributeID IS NULL,sum(itd.itd_quantity),
-					truncate(sum(itd.itd_quantity)/count(dl.attributeID),0))
-					as itd_quantity, itd_itm_id, itd_itl_id, itl_flagText ";
+			$sql .= ", if(dl.attributeID IS NULL,sum(itd.itd_quantity),"
+					."truncate(sum(itd.itd_quantity)/count(dl.attributeID),0)) "
+					."as itd_quantity, itd_itm_id, itd_itl_id, itl_flagText, itd_singleton ";
 		} else if (count($this->droppedIDarray)) {
-			$sql .= ", if(dl.attributeID IS NULL,sum(itd.itd_quantity),
-					truncate(sum(itd.itd_quantity)/count(dl.attributeID),0))
-					as itd_quantity, itd_itm_id, itd_itl_id, itl_flagText ";
+			$sql .= ", if(dl.attributeID IS NULL,sum(itd.itd_quantity),"
+					."truncate(sum(itd.itd_quantity)/count(dl.attributeID),0)) "
+					."as itd_quantity, itd_itm_id, itd_itl_id, itl_flagText, itd_singleton ";
 		}
 
 		$sql .= "from kb3_invtypes inv
-				left join kb3_dgmtypeattributes dga on dga.typeID=inv.typeID and dga.attributeID=633
-				left join kb3_dgmtypeattributes dgb on dgb.typeID=inv.typeID and dgb.attributeID=422
-				left join kb3_item_price itp on itp.typeID=inv.typeID
-				left join kb3_item_types on inv.groupID=itt_id
-				left join kb3_dgmtypeattributes dc on dc.typeID = inv.typeID AND dc.attributeID IN (128)
-				left join kb3_dgmtypeattributes dl on dl.typeID = inv.typeID AND dl.attributeID IN (137,602) ";
+                            left join kb3_dgmtypeattributes dga on dga.typeID=inv.typeID and dga.attributeID=633
+                            left join kb3_dgmtypeattributes dgb on dgb.typeID=inv.typeID and dgb.attributeID=422
+                            left join kb3_item_price itp on itp.typeID=inv.typeID
+                            left join kb3_item_types on inv.groupID=itt_id
+                            left join kb3_dgmtypeattributes dc on dc.typeID = inv.typeID AND dc.attributeID IN (128)
+                            left join kb3_dgmtypeattributes dl on dl.typeID = inv.typeID AND dl.attributeID IN (137,602) ";
 
 		if (count($this->destroyedIDarray)) {
-			$sql .= "join kb3_items_destroyed itd on inv.typeID = itd_itm_id
-					and itd_kll_id in (
-					".implode(',', $this->destroyedIDarray).")
-					left join kb3_item_locations itl
-					on (itd.itd_itl_id = itl.itl_flagID )";
+			$sql .= "join kb3_items_destroyed itd on inv.typeID = itd_itm_id "
+					."and itd_kll_id in ("
+					.implode(',', $this->destroyedIDarray).") "
+					."left join kb3_item_locations itl "
+					."on (itd.itd_itl_id = itl.itl_flagID )";
 		} else if (count($this->droppedIDarray)) {
-			$sql .= "join kb3_items_dropped itd
-					on inv.typeID = itd_itm_id and itd_kll_id in (
-					".implode(',', $this->droppedIDarray).")
-					left join kb3_item_locations itl
-					on (itd.itd_itl_id = itl.itl_flagID ) ";
+			$sql .= "join kb3_items_dropped itd "
+					."on inv.typeID = itd_itm_id and itd_kll_id in ("
+					.implode(',', $this->droppedIDarray).") "
+					."left join kb3_item_locations itl "
+					."on (itd.itd_itl_id = itl.itl_flagID ) ";
 		} else {
 			$sql .= "where inv.typeID in (".implode(',', $this->itemarray).") ";
 		}
 
 		if (count($this->destroyedIDarray) || count($this->droppedIDarray)) {
-			$sql .= "group by itd.itd_itm_id, itd.itd_itl_id "
+			$sql .= "group by itd.itd_itm_id, itd.itd_itl_id, itd.itd_singleton "
 					."order by itd.itd_itl_id ";
 		}
 
